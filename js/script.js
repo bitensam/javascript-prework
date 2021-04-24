5448d867ace425a15a40f92742917ed8fdaad13a
Jakub Wojcik <j.wojcik1994@gmail.com>
2021-04-24 20:50:18 +0200

    Add playerMove else / if statemant

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to przegrywasz!');
 
+//ruch komputer
 
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
@@ -18,3 +19,19 @@ if (randomNumber == '1') {
     computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+//ruch gracza
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nożyce';
+} else {
+    playerMove = 'kamień';
+}
+printMessage('Twój ruch: ' + playerMove);
